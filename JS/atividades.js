11951f3d9cf27771da563342ed5224267fa897c3
EDUARDO SANTOS DA SILVA <eduardo.ssilva36@senacsp.edu.br>
2024-06-17 22:04:21 -0300

    Segundo Commit

--- JS/atividades.js
+++ JS/atividades.js
@@ -1,1 +1,17 @@
+function bissexto() {
+    var ano; //Declarar a variavel
 
+
+    //Coletar os dados do formulário
+    ano = parseInt(document.getElementById("ano").value);
+
+    //Realizar Conta
+
+    if(ano % 4 == 0){
+        ano = "O Ano é bissexto"
+    }else{
+        ano = "O Ano não é Bissexto"
+    }
+
+    document.getElementById("resultado").innerHTML = ano;
+}
